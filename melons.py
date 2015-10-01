37c73e35655c6af58b8390eb7dd1ed2762fca844
Hackbright Student <info@hackbrightacademy.com>
2015-09-30 17:48:06 -0700

    wrote classes and get_price methods for cataloupe and casaba

--- melons.py
+++ melons.py
@@ -31,3 +31,101 @@ class WatermelonOrder(object):
         price = self.get_price(qty)
 
         print "${:.2f}".format(price)
+
+
+
+
+
+
+
+class CantaloupeOrder(object): 
+    species = "Cantaloupe"
+    color = "tan"
+    imported = False
+    shape = "natural"
+    seasons = ["Spring", "Summer"]
+
+    def get_price(self, qty):
+        """Calculate the price of the cantaloupe order based on quantity."""
+        total = BASE_COST * float(qty)
+        if qty >= 5:
+            total /= 2
+        return total
+
+    def print_price(self, qty):
+        """Prints get_price"""
+        price = self.get_price(qty)
+        print "${:.2f}".format(price)
+
+
+
+
+
+
+
+
+
+
+class CasabaOrder(object):
+    species = "Casaba"
+    color = "green"
+    imported = True
+    shape = "natural"
+    seasons = ["Spring", "Summer", "Fall", "Winter"]
+
+    def get_price(self, qty):
+        """Calulates a total cost of Casaba for qty Casaba."""
+        
+        total = (BASE_COST + 1.00) * qty * 1.5
+        return total
+
+    def print_price(self, qty):
+        """Passes qty to get_price and prints pretty version of what's returned"""
+
+        price = self.get_price(qty)
+        print "${:.2f}".format(price)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
